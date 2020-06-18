9a09818f9c92725dce7f825292b7c25419763d65
Kelly Klein <kmklein2@dmacc.edu>
2020-06-17 20:12:51 -0500

    tests done

---
+++ more_functions/validate_input_in_functions.py
@@ -0,0 +1,20 @@
+"""
+Program: validate_input_in_functions.py
+Author: Kelly Klein
+Last date modified: 6/17/2020
+This program will .
+"""
+
+
+def score_input(test_name, test_score=0, invalid_message='Invalid test score, try again!'):
+    """
+Use reST style.
+:param test_name: input for name of test
+:param test_score: score the student got on test
+:param invalid_message:
+:return: test_name, test_score
+raises keyError: raises an exception
+"""
+
+    return
+    pass
--- test_functions/test_valid_input_in_functions.py
+++ test_functions/test_valid_input_in_functions.py
@@ -1,9 +1,25 @@
 import unittest
+from more_functions import validate_input_in_functions as validate
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+
+    def test_score_input_test_name(self):
+
+        self.assertEqual('Kelly test 1', validate.score_input('Kelly test 1'))
+
+    def test_score_input_test_score_valid(self):
+        self.assertEqual('Kelly test 2', 95, validate.score_input('Kelly test 2', 95))
+
+    def test_score_input_test_score_below_range(self):
+        self.assertTrue(validate.score_input.test_score >= 0)
+
+    def test_score_input_test_score_above_range(self):
+        self.assertTrue(validate.score_input.test_score <= 100)
+
+
+    def test_test_score_non_numeric(self):
+        assert type(self) == int, 'Invalid test score, try again!'
 
 
 if __name__ == '__main__':
